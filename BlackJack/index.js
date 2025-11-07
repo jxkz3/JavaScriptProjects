3f9b7657c6709b3e89900a6752aad9cac8d4cea2
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-07 16:40:51 +0530

    Quick update

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,33 +1,45 @@
- let firstCard = 10
-  let secondCard = 11
-  let sum = firstCard + secondCard 
+let firstCard = 7;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let cards = [firstCard, secondCard];
 
-  let message = ""
+let message = "";
 
 function startGame() {
-  const messageEl = document.getElementById("message-el")
-  const sumEl = document.getElementById("sum-el")
-  const cardEl = document.querySelector("#card-el")
-
- 
-
-  sumEl.textContent = "Sum: " + sum
-  cardEl.textContent = "Card :" + firstCard + "+ " + secondCard
-
-  if (sum <= 20) {
-      message = "You still have a move"
-  } else if (sum === 21) {
-      message = "You win!"
-      hasBlackJack = true
-  } else {
-      message = "You lose!"
-      isAlive = false
-  }
-
-  messageEl.textContent = message
-  
+  renderGame();
 }
 
-function newCard(){
-   console.log("jas")
+function renderGame() {
+  const messageEl = document.getElementById("message-el");
+  const sumEl = document.getElementById("sum-el");
+  const cardEl = document.querySelector("#card-el");
+
+  cardEl.textContent = "Card :";
+
+  sumEl.textContent = "Sum: " + sum;
+
+  for (let i = 0; i < cards.length; i++) {
+    cardEl.textContent += cards[i] + " ";
+  }
+
+  if (sum <= 20) {
+    message = "You still have a move";
+  } else if (sum === 21) {
+    message = "You win!";
+    hasBlackJack = true;
+  } else {
+    message = "You lose!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = 4;
+
+  cards.push(card);
+  sum = sum + card;
+  console.log(cards);
+  renderGame();
 }
