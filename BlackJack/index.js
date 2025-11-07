a6b73746e91f7c100fc275906dddf0b99737ffa4
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-07 11:13:18 +0530

    defined start function

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,19 +1,33 @@
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
-let hasBlackJack = false
-let isAlive = true
-// 1. Declare a variable called message and assign its value to an empty string
+ let firstCard = 10
+  let secondCard = 11
+  let sum = firstCard + secondCard 
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-    isAlive = false
+  let message = ""
+
+function startGame() {
+  const messageEl = document.getElementById("message-el")
+  const sumEl = document.getElementById("sum-el")
+  const cardEl = document.querySelector("#card-el")
+
+ 
+
+  sumEl.textContent = "Sum: " + sum
+  cardEl.textContent = "Card :" + firstCard + "+ " + secondCard
+
+  if (sum <= 20) {
+      message = "You still have a move"
+  } else if (sum === 21) {
+      message = "You win!"
+      hasBlackJack = true
+  } else {
+      message = "You lose!"
+      isAlive = false
+  }
+
+  messageEl.textContent = message
+  
 }
 
-// 3. Log it out!
+function newCard(){
+   console.log("jas")
+}
