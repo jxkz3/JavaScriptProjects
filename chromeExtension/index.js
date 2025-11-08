6ba37453ef1ea32e959c8a90c1932b98ae5fdeef
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-08 18:08:27 +0530

    Quick update

---
+++ chromeExtension/index.js
@@ -0,0 +1,6 @@
+let leadsEl = document.getElementById("leads-el");
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
+  leadsEl.textContent = "hello my name is cat";
+});
