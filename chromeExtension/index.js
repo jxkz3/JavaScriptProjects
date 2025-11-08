fdbed725ff25ef0ec7f8a08445ffa11716c80924
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-08 19:54:39 +0530

    Second Quick update

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,6 +1,19 @@
-let leadsEl = document.getElementById("leads-el");
-let inputBtn = document.getElementById("input-btn");
+//let leads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const leadsEl = document.getElementById("leads-el");
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el").value;
 
 inputBtn.addEventListener("click", function () {
-  leadsEl.textContent = "hello my name is cat";
+  myLeads.push(inputEl);
+  console.log(myLeads);
+  inputEl.value = "";
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+  // leadsEl.innerHTML += "<li> " + myLeads[i] + " </li>";
+
+  const li = document.createElement("li");
+  li.textContent = myLeads[i];
+  leadsEl.append(li);
+}
