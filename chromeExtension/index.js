410442146a553d565a0427b42ea8a1ef48e5d46f
Jxkz <107039597+jxkz3@users.noreply.github.com>
2025-11-11 16:33:58 +0530

    Add local storage support and clear data button

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -2,12 +2,20 @@ let myLeads = [];
 const leadsEl = document.getElementById("leads-el");
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
+const clearBtn = document.getElementById("clear-btn");
+
+let leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsLocalStorage) {
+  myLeads = leadsLocalStorage;
+  renderLeads();
+}
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
   console.log(myLeads);
-  localStorage.setItem("myleads", JSON.stringify(myLeads));
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
 
   renderLeads();
 });
@@ -16,7 +24,19 @@ function renderLeads() {
   let listItems = "";
 
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li> <a href="${myLeads}">  ${myLeads[i]}  </a> </li>`;
+    listItems += `
+      <li>    
+            <a href="${myLeads}" target="_blank"> 
+               ${myLeads[i]}  
+            </a> 
+      </li>`;
   }
+
   leadsEl.innerHTML = listItems;
 }
+
+clearBtn.addEventListener("click", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
